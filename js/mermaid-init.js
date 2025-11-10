b0ccb7f9a0cf0dc1fe965991e4c900a4689e9375
Raj-G07 <Raj-G07@users.noreply.github.com>
2025-11-10 15:25:49 +0000

    deploy: 73b3c76b7d9ce39d5ce382a781e648cd0603806a

---
+++ js/mermaid-init.js
@@ -0,0 +1,12 @@
+mermaid.initialize({
+  startOnLoad: true,
+  theme: "base",
+  themeVariables: {
+    primaryColor: "var(--mermaid-node-bg)",
+    primaryBorderColor: "var(--mermaid-node-border)",
+    primaryTextColor: "var(--mermaid-text-color)",
+    edgeLabelBackground: "var(--mermaid-node-bg)",
+    lineColor: "var(--mermaid-edge-color)",
+    fontFamily: "var(--mermaid-font-family)"
+  }
+});
